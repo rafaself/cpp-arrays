8764793a972c496a165f54fd3618cda8eb7874ad
rafa <rafaondjango@gmail.com>
2026-02-13 23:50:11 -0300

    style: format code for consistency and readability

--- 01_basic_c_array.cpp
+++ 01_basic_c_array.cpp
@@ -2,21 +2,25 @@
 #include <iostream>
 #include <iterator>
 
-int main() {
-  int a[5];  // C array (fixed size, known at compile time)
+int main()
+{
+  int a[5];
 
-  for (std::size_t i = 0; i < std::size(a); ++i) {
-    a[i] = static_cast<int>((i + 1) * 10);  // 10, 20, 30, 40, 50
+  for (std::size_t i = 0; i < std::size(a); ++i)
+  {
+    a[i] = static_cast<int>((i + 1) * 10);
   }
 
   std::cout << "Array a = [ ";
-  for (std::size_t i = 0; i < std::size(a); ++i) {
+  for (std::size_t i = 0; i < std::size(a); ++i)
+  {
     std::cout << a[i] << ' ';
   }
   std::cout << "]\n";
 
   int sum = 0;
-  for (std::size_t i = 0; i < std::size(a); ++i) {
+  for (std::size_t i = 0; i < std::size(a); ++i)
+  {
     sum += a[i];
   }
   std::cout << "Sum = " << sum << "\n";
